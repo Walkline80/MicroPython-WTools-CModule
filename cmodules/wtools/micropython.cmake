4a736d92f7f389c0bb4027b7cc016967bea80486
Walkline Wang <walkline@163.com>
2024-05-01 21:06:08 +0800

    cmake: 增加 `classes` 相关路径和文件
    Signed-off-by: Walkline Wang <walkline@163.com>

--- cmodules/wtools/micropython.cmake
+++ cmodules/wtools/micropython.cmake
@@ -4,6 +4,14 @@ add_library(usermod_wtools INTERFACE)
 # Add our source files to the lib
 target_sources(usermod_wtools INTERFACE
     ${CMAKE_CURRENT_LIST_DIR}/modwtools.c
+    ${CMAKE_CURRENT_LIST_DIR}/classes/arraytool.c
+    ${CMAKE_CURRENT_LIST_DIR}/classes/colortool.c
+)
+
+# Add the current directory as an include directory.
+target_include_directories(usermod_wtools INTERFACE
+    ${CMAKE_CURRENT_LIST_DIR}
+    ${CMAKE_CURRENT_LIST_DIR}/classes
 )
 
 # Link our INTERFACE library to the usermod target.
--- cmodules/wtools/micropython.mk
+++ cmodules/wtools/micropython.mk
@@ -1,4 +1,11 @@
-SMARTCONFIG_MOD_DIR := $(USERMOD_DIR)
+WTOOLS_MOD_DIR := $(USERMOD_DIR)
 
 # Add all C files to SRC_USERMOD.
-SRC_USERMOD_C += $(SMARTCONFIG_MOD_DIR)/wtools.c
+SRC_USERMOD_C += $(WTOOLS_MOD_DIR)/wtools.c
+SRC_USERMOD_LIB_C += $(WTOOLS_MOD_DIR)/classes/arraytool.c
+SRC_USERMOD_LIB_C += $(WTOOLS_MOD_DIR)/classes/colortool.c
+
+# We can add our module folder to include paths if needed
+# This is not actually needed in this example.
+CFLAGS_USERMOD += -I$(WTOOLS_MOD_DIR)
+CFLAGS_USERMOD += -I$(WTOOLS_MOD_DIR)/classes
